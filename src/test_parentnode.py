d00716d5e60f81f3e65b47c73fb6f470f6f98510
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-11 11:35:31 +0700

    two more unit test for parent node

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -17,3 +17,28 @@ class TestParentNode(unittest.TestCase):
         actual = parent_node.to_html()
         expected = "<p><b>Bold text</b>Normal text<span style = 'color: red;' >Red span text</span></p>"
         self.assertEqual(actual, expected)
+
+    def test_parent_noprops(self):
+        parent_node = ParentNode(
+            "h1", [LeafNode(None, "Heading"), LeafNode("span", 1)], None
+        )
+
+        actual = parent_node.to_html()
+        expected = "<h1>Heading<span>1</span></h1>"
+
+        self.assertEqual(actual, expected)
+
+    def test_parent_both_props(self):
+        parent_node = ParentNode(
+            "div",
+            [
+                LeafNode("h1", "Heading 1", {"style": "color: purple;"}),
+                LeafNode("p", "First paragraph"),
+                LeafNode("p", "Second paragraph"),
+            ],
+            {"align": "center", "padding": "10px"},
+        )
+
+        actual = parent_node.to_html()
+        expected = "<div align = 'center' padding = '10px' ><h1 style = 'color: purple;' >Heading 1</h1><p>First paragraph</p><p>Second paragraph</p></div>"
+        self.assertEqual(actual, expected)
