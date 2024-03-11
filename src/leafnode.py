20d2a440e1aae66a03661dbcac86a77b70e697a8
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-11 10:37:00 +0700

    test parent node

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,11 +3,16 @@ from htmlnode import HTMLNode
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, props=props)
+        super().__init__(tag, value, children=None, props=props)
 
     def to_html(self):
         if self.value is None:
             raise ValueError("Value is required for LeafNode")
 
-        props_html = f"{self.props_to_html()}" if self.props else ""
-        return f"<{self.tag} {props_html}>{self.value}</{self.tag}>" if self.tag else f"{self.value}"
+        if self.tag is None:
+            return f"{self.value}"
+
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/parentnode.py
+++ src/parentnode.py
@@ -2,19 +2,19 @@ from htmlnode import HTMLNode
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag, children=None, props=None):
-        super().__init__(tag=tag, children=children, props=props)
-        if not self.children or len(self.children) == 0:
-            raise ValueError("children attribute must not be empty list")
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
 
     def to_html(self):
         if not self.tag:
             raise ValueError("tag attribute is required")
+        if not self.children:
+            raise ValueError("children attribute is required")
 
-        opening_tag = (
-            f"<{self.tag} {self.props_to_html()}>" if self.props else f"<{self.tag}>"
-        )
-        closing_tag = f"</{self.tag}>"
-        children_html = "".join(child.to_html() for child in self.children)
+        children_html = f"<{self.tag} {self.props_to_html()}>"
 
-        return f"{opening_tag}{children_html}{closing_tag}"
+        for child in self.children:
+            children_html += child.to_html()
+
+        children_html += f"</{self.tag}>"
+        return children_html
